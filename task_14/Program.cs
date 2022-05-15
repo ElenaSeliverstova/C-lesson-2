68bbd493014939a2daefef330738de3395f836d2
Lena <eseliver@icloud.com>
2022-05-15 15:41:03 +0300

    14

---
+++ task_14/Program.cs
@@ -0,0 +1,5 @@
+﻿// Вводим число, проверяем кратно ли одно одновремено 7 и 23
+Console.Write("Введите целое число => ");
+int num = int.Parse(Console.ReadLine ());
+if (num % 7 == 0 && num % 23 == 0) Console.WriteLine("Yes");
+else Console.WriteLine("No");
