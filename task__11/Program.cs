f57b81ff1eb033ee6a2d9492291709db33fe48c3
Lena <eseliver@icloud.com>
2022-05-15 15:40:23 +0300

    13

---
+++ task__11/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите программу, которая выводит случайное 3-х значное число и удаляет вторую цифру этого числа
+int num = new Random().Next(100, 1000);
+
+Console.WriteLine("Число = " + num);
+string text = num.ToString();
+Console.WriteLine("Модификация трехзначного числа: " + num + "-> " + text[0] + text [2]);
+
+int num2 = (num / 100) * 10 + num % 10;
+Console.WriteLine("Модификация трехзначного числа: "+ num + "-> " + num2);
