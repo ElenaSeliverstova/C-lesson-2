a310e70c6680bdcc831f64eef50a7d2fbff73270
Lena <eseliver@icloud.com>
2022-05-15 15:41:36 +0300

    15

---
+++ task_16/Program.cs
@@ -0,0 +1,14 @@
+﻿// на вход 2 числа, проверить является ли одно квадартом другого
+Console.Write("Введите целое число 1 => ");
+int num1 = int.Parse(Console.ReadLine ());
+Console.Write("Введите целое число 2 => ");
+int num2 = int.Parse(Console.ReadLine ());
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Yes");
+ }
+else
+{
+    Console.WriteLine("No");
+ }
