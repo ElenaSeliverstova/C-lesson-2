de3ac1d6cdc8c4795fd1b523d3974cefa3928fdb
Lena <eseliver@icloud.com>
2022-05-16 21:04:08 +0300

    18

--- task_17/Program.cs
+++ task_17/Program.cs
@@ -1,3 +1,23 @@
 ﻿// программа принимает на вход координаты точки, где x и y не равны нулю 
 // и выдает номер четверти плоскости, в которой находится точка
 
+Console.Write("введите значение Х: ");
+int x = int.Parse(Console.ReadLine ());
+Console.Write("введите значение Y: ");
+int y = int.Parse(Console.ReadLine ());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1 четверть");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2 четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4 четверть");
+}
---
+++ task_18/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа по заданному номеру четверти 
+// показывает диапазон возможных координат точек в этой четверти (x и y)
+
+Console.Write("введите цифру от 1 до 4: ");
+int xy = int.Parse(Console.ReadLine ());
+if (xy == 1)
+{
+    Console.WriteLine("обе координаты плюс");
+}
+if (xy == 2)
+{
+    Console.WriteLine("X минус, Y плюс");
+}
+if (xy == 3)
+{
+    Console.WriteLine("X минус, Y минус");
+}
+if (xy == 4)
+{
+    Console.WriteLine("X плюс, Y минус");
+}
