b9eb3ae722d8c6c46ed3572e5cdef8b8e410ba06
Lena <eseliver@icloud.com>
2022-05-16 22:02:58 +0300

    22

---
+++ task_22/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа принимает на вход число N
+// выдает таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("введите число N");
+int lenght = Convert.ToInt32(Console.ReadLine ());
+
+for (int i = 1; i <= lenght; i++) 
+{
+Console.Write(i * i + " ");
+}
