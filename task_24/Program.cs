f69d6ae92d32c11c3bc7007fc79e18a088d04823
Lena <eseliver@icloud.com>
2022-05-21 10:26:58 +0300

    24

---
+++ task_24/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 24: сумма чисел от 1 до А
+Console.Write("Введите число А = ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+    sum = sum + i;
+    // sum += i;
+}
+ Console.WriteLine("Сумма чисел от 1 до " + number + " = " + sum);
