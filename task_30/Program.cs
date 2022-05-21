43177fcca4e0f7d92f8b649b260d3050e284bc87
Lena <eseliver@icloud.com>
2022-05-21 12:00:33 +0300

    30

---
+++ task_30/Program.cs
@@ -0,0 +1,14 @@
+﻿// создать массив из 8 чисел, заполнить значениями 0 и 1 в произвольном порядке
+
+int[] arr = new int[8];
+int[] PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        Console.Write(array[i]);
+    }
+    return array;
+}
+PrintArray(arr);
