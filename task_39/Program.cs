cc7598c02e60f5604c6612508b3c383acf5da952
Lena <eseliver@icloud.com>
2022-05-28 10:31:43 +0300

    39

---
+++ task_39/Program.cs
@@ -0,0 +1,18 @@
+﻿// Разворот массива
+Console.Write("Введите элементы массива через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.WriteLine(String.Join(" ", array));
+
+int size = array.Length;
+int index1 = 0;
+int index2 = size - 1;
+
+while (index1 < index2)
+{
+    int temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+    index1++;
+    index2--;
+}
+Console.WriteLine(String.Join(" ", array));
