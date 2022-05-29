ba7a89ff87d408f713090ec4c7fba7e1c5356a68
Lena <eseliver@icloud.com>
2022-05-29 12:07:06 +0300

    49

---
+++ task_49/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+
+int [,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 9);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetNewArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 2)
+        {
+            array[i, j] *= array[i, j];
+        }
+    }
+    return array;
+}
+
+int[,] arrayResult = GetArray(3, 4);
+PrintArray(arrayResult);
+Console.WriteLine();
+PrintArray(GetNewArray(arrayResult));
