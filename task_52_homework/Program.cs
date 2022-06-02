3e1ecd8c3321002e8d8f462aebe3e0591100b4f8
Lena <eseliver@icloud.com>
2022-06-02 17:27:30 +0300

    52

---
+++ task_52_homework/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 9);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] GetAverageClmn(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            result[j] = sum / array.GetLength(1);
+        }
+    }
+    return result;
+}
+void PrintNewArray(double[] array)
+{
+    System.Console.WriteLine("Средние значения по столбцам:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}  ");
+    }
+}
+
+int[,] arrayResult = GetArray(4, 4);
+Console.WriteLine("Исходный массив: ");
+PrintArray(arrayResult);
+double[] averageclmn = GetAverageClmn(arrayResult);
+PrintNewArray(averageclmn);
