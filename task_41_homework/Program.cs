76f18b5cd6fc8ef573313afc4a21473845bb4958
Lena <eseliver@icloud.com>
2022-06-03 13:58:49 +0300

    41

--- task_41_homework/Program.cs
+++ task_41_homework/Program.cs
@@ -1,20 +1,20 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-Console.WriteLine("Введите любое количество положительных и отрицательных чисел: ");
+Console.Write("Введите любое количество положительных и отрицательных чисел: ");
 int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
 Console.WriteLine($"Ваш массив: {String.Join(" ", array)}");
 
-// int CountOfPositive(int [] arr, int count) // я не понимаю:(
-// {
-int size = array.Length;
-int result = 0;
-for (int i = 0; i < size; i++)
+int CountOfPositive(int[] array)
 {
-    if (array[i] > 0) result = result + 1;
+    int size = array.Length;
+    int result = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > 0)
+            result = result + 1;
+    }
+    return result;
 }
-Console.Write($"Количество положительных чисел: {result}");
-// }
-// int res = int CountOfPositive (int [] array, count);
-
-
+  int arrayResult = CountOfPositive(array);
+  Console.Write($"Количество положительных чисел: {arrayResult}");
