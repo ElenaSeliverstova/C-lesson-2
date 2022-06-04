c13277884c232bd8c3be3196326f1cdfb8746fa4
Lena <eseliver@icloud.com>
2022-06-04 14:14:55 +0300

    55

---
+++ task_55/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 9);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void TransposeMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = i; j < array.GetLength(1); j++)
+        {
+            int tmp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = tmp;
+        }
+    }
+}
+
+int[,] matrix = GetArray(3, 3);
+PrintArray(matrix);
+Console.WriteLine();
+
+if (matrix.GetLength(0) == matrix.GetLength(1))
+{
+    TransposeMatrix(matrix);
+    PrintArray(matrix);
+}
+else System.Console.WriteLine("Данную матрицу транспонировать невозможно!" + "\n");
