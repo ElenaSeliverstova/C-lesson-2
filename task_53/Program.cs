cb53d430ba5ac21907a5dc4b0a95385dfba600ec
Lena <eseliver@icloud.com>
2022-06-04 12:38:31 +0300

    53

---
+++ task_53/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+// P.S способ работает только на квадратные массивы
+
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 9);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeRowArray(int[,] arr)
+{
+    int temp = 0;
+    for (int j = 0; j < arr.GetLength(0); j++)
+    {
+        temp = arr[0, j];
+        arr[0, j] = arr[arr.GetLength(0) - 1, j];
+        arr[arr.GetLength(0) - 1, j] = temp;
+    }
+    return arr;
+}
+
+int[,] arrayResult = GetArray(7, 7);
+PrintArray(arrayResult);
+Console.WriteLine();
+int[,] newarr = ChangeRowArray(arrayResult);
+PrintArray(newarr);
