35f6fdfba4e7c8845ddb57d75f4b04ac472a6bda
Lena <eseliver@icloud.com>
2022-06-09 18:07:48 +0300

    67

---
+++ task_67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+Console.Write("Введите число: ");
+int sum = int.Parse(Console.ReadLine());
+
+int SumNumber(int number)
+{
+    int result = 0;
+    if (number == 0) return 0;
+    result = number % 10 + SumNumber(number/10);
+    return result;
+}
+
+Console.WriteLine(SumNumber(sum));
