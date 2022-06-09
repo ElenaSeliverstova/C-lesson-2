8c5b1f06ca6088af77b2e53a204d2b96b3099bef
Lena <eseliver@icloud.com>
2022-06-09 19:27:18 +0300

    68

---
+++ task_68_homework/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+Console.Write("Введите M: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int numN = int.Parse(Console.ReadLine());
+
+int Ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ack(m - 1, 1);
+    }
+    else
+    {
+        return Ack(m - 1, Ack(m, n - 1));
+    }
+
+}
+
+Console.Write(Ack(numM, numN));
+
+
+
+
+
